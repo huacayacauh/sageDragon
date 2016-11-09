e8badba9b85ac3aebfc099281d25a06c0aefc43e
Scra <albanbertolini@gmail.com>
2016-11-09 23:44:06 +0100

    afficher mathjax

--- extensions/sagedragon.js
+++ extensions/sagedragon.js
@@ -1,28 +1,40 @@
 /*
-Add this file to $(ipython locate)/nbextensions/sageDragon.js
-And load it with:
+ Add this file to $(ipython locate)/nbextensions/sageDragon.js
+ And load it with:
+ 
+ require(["nbextensions/sageDragon"], function (sageDragon_extension) {
+ console.log('sageDragon extension loaded');
+ sageDragon_extension.load_ipython_extension();
+ });
+ 
+ */
 
-require(["nbextensions/sageDragon"], function (sageDragon_extension) {
-    console.log('sageDragon extension loaded');
-    sageDragon_extension.load_ipython_extension();
-});
 
-*/
-var activ = false;
-define( function () {
+define(function () {
 
+
+    /*Display latex et clear outputs*/
     var sageDragon_notebook = function () {
+        Jupyter.notebook.kernel.execute("%display latex");
+        Jupyter.dialog.modal({
+            title: "sageDragon activé",
+            body: "",
+            buttons: {"ok": {}}
+        });
 
-	if(activ == false){
-	       Jupyter.notebook.kernel.execute("%display latex");
-		activ = true;
-	       alert("SageDragon est activé");
-	}else{
-		alert("SageDragon est déja activé");	
-	}
+        // on cherche le bouton run cell et on ajoute l'action onclick
+        //PROBLEME IL FAUT RÉUSIR A AFFICHER APRES LE OUTPUT DE MATHJAX ET NON AVANT
+        $('[title ="run cell, select below"]').click(function () {
+            alert($(".MJX_Assistive_MathML:last").text())
+        });
     };
-    
-   
+
+
+    var display_output = function () {
+        $(".output_result").text(function (index, actuel) {
+            alert('Output ' + (index + 1) + ' : ' + actuel)
+        });
+    };
 
     var sageDragon_button = function () {
         if (!IPython.toolbar) {
@@ -32,21 +44,39 @@ define( function () {
         if ($("#sageDragon_notebook").length === 0) {
             IPython.toolbar.add_buttons_group([
                 {
-                    'label'   : 'Use sageDragon',
-                    'icon'    : 'fa-play-circle',
-                    'callback':	sageDragon_notebook,
-                    'id'      : 'sageDragon_notebook'
+                    'label': 'Use sageDragon',
+                    'icon': 'fa-play-circle',
+                    'callback': sageDragon_notebook,
+                    'id': 'sageDragon_notebook'
                 },
             ]);
         }
     };
-    
+
     var load_ipython_extension = function () {
         sageDragon_button();
+        activate_extension(self);
     };
-    
+
+    var activate_extension = function (self) {
+        //Install JS extensions
+        path = os.path.abspath(os.path.join.dirname("notebook", os.path.padir));
+        install_nbextension(path, overwrite = True, user = True);
+        js_cm = ConfigManager();
+        js_cm.update('notebook', {"load_extensions": {'sageDragon/static/js/notebook ': True}})
+    };
     return {
-        load_ipython_extension : load_ipython_extension,
+        load_ipython_extension: load_ipython_extension
     };
-    
+
+
+
 });
+
+
+
+
+
+
+
+
