2ae4562141d470db3063295adb5544d0565b6977
scra3 <albanbertolini@gmail.com>
2016-12-01 13:48:10 +0100

    clean repo

--- extensions/sagedragon.js
+++ extensions/sagedragon.js
@@ -29,31 +29,44 @@ function () {
 		return resultat;
 	};
 
-	// Permet de vérifier si une formule est valide
+        /*
+        Cette fonction prend en paramètre une cellule et une opération. L’opération contient ce qu’il faut exécuter avec le kernel, 
+        et elle est définie auparavant dans les différente fonctions qui appeleront check_and_execute. 
+        */
 	var check_and_execute_formule = function (cell, operation)
 	{
+		//Exécute une commande "opération" via le kernel
+                //cell.get_callbacks() permet de récupérer le résultat et de le mettre dans l'output de la cellule
 		Jupyter.notebook.kernel.execute(operation, cell.get_callbacks(), {silent:false} );
-		setTimeout(function(){
+		//On attend le résultat
+                setTimeout(function(){
+			//si output contient une erreur on vide l'output et on indique que la formule n'est pas valide
 			if(cell.output_area.outputs[0].traceback != undefined){			
 				cell.output_area.clear_output();
 				alert("Formule non valide");		
 			}	
 		}, 200);
 	}
-	
+
+
 
 	//developpe les formules
 	var expand = function (cell) 
 	{
+                //On récupère ce qu'il y dans l'input de la cellule
 		var text = cell.get_text();
+                //si l'input est vide on indique que le formule n'est pas valide
 		if(text.trim()!="")
 		{
+			//formatage de la chaine en fonction de l'opération à effectuer
 			var operation = "expand("+text+");"
-			/*Création des variables*/
+			//Création des variables
 	        	create_var(cell);
+                        //si l'output n'est pas vide, on le vide
 	        	if(cell.output_area.outputs[0]!=undefined){
 	        		cell.output_area.clear_output();
-			}	
+			}
+                //vérification de la formule et exécution de la commande	
 		check_and_execute_formule(cell, operation);	
 	        }
 	 	else
@@ -65,15 +78,20 @@ function () {
 	// Fonction pour factoriser
 	var factor = function (cell) 
 	{
+		//On récupère ce qu'il y dans l'input de la cellule
 		var text = cell.get_text();
+		//si l'input est vide on indique que le formule n'est pas valide
 		if(text.trim()!="")
 		{
+			//formatage de la chaine en fonction de l'opération à effectuer			
 			var operation = "factor("+text+");";
-			/*Création des variables*/
+			//Création des variables
 	        	create_var(cell);
+			//si l'output n'est pas vide, on le vide
 	        	if(cell.output_area.outputs[0]!=undefined){
 	        		cell.output_area.clear_output();
 			}	
+		//vérification de la formule et exécution de la commande
 		check_and_execute_formule(cell, operation);	
 	        }
 	 	else
@@ -88,9 +106,11 @@ function () {
 
 	var solve = function (cell) 
 	{	
+	        var valide_function_to_solve = /^([a-z]|([0-9]*)+)(([+\-*\/]([a-z]|([0-9]*)+))*)?/g;
+		
 		// contenu input
 	        var text = cell.get_text();
-
+	        var valide = text.replace(valide_function_to_solve, "");
 		if(text.trim()!="")
 		{
 			var vars; // variables en string
@@ -98,33 +118,30 @@ function () {
 			var inputTab =[]; // cellule Unique dans un tab
 			var find; // permet de savoir si la variable est unique ou pas
 			var buttons = {} ;//Stocker les boutons dans la boite de dialog
-			
+
+
 			/*Création des variables*/
 			    vars = create_var(cell);
 				if(cell.output_area.outputs[0]!=undefined){
 	        			cell.output_area.clear_output();
 				}
-			if(vars.length > 0){
-				/*Propositions des variables*/
-				 inputTab = variablesUnique(vars);
+			/*Propositions des variables*/
+			 inputTab = variablesUnique(vars);
 
-				// On créé notre input
-				input = "<div class ='col-xs-12'><b>Choose the variable in order to solve :<br><center>"+text+"</center></b></div>";
+			// On créé notre input
+			input = "<div class ='col-xs-12'><b>Choose the variable in order to solve :<br><center>"+text+"</center></b></div>";
 		
-				// création des boutons
+			// création des boutons
 
-				var val = [];
-				for(i = 0 ; i < inputTab.length; i ++){
-					val[i] = inputTab[i];
-					buttons[val[i]] = factoryButtons(val[i],text,cell);	 
-				}
-
-				// creation de la boite de dialog
-				var options = {height: 'auto',width: 'auto', buttons:buttons}
-				createDialog("Choose your variable",input,options);
-			}else{
-				alert("Formule non valide");			
+			var val = [];
+			for(i = 0 ; i < inputTab.length; i ++){
+				val[i] = inputTab[i];
+				buttons[val[i]] = factoryButtons(val[i],text,cell);	 
 			}
+
+			// creation de la boite de dialog
+			var options = {height: 'auto',width: 'auto', buttons:buttons}
+			createDialog("Choose your variable",input,options);
 		}else{
 	        	alert("Formule non valide");
 		}
@@ -167,19 +184,26 @@ function () {
 		check_and_execute_formule(cell, operation);	
 	};
 
+
+                        
  
 	//Donne la dérivée
 	var diff = function (cell) 
 	{
+		//On récupère ce qu'il y dans l'input de la cellule
 		var text = cell.get_text();
+		//si l'input est vide on indique que le formule n'est pas valide
 		if(text.trim()!="")
 		{
+			//formatage de la chaine en fonction de l'opération à effectuer	
 			var operation = "diff("+text+");"
-			/*Création des variables*/
+			//Création des variables 
 	        	create_var(cell);
+			//si l'output n'est pas vide, on le vide
 	        	if(cell.output_area.outputs[0]!=undefined){
 	        		cell.output_area.clear_output();
-			}	
+			}
+		//vérification de la formule et exécution de la commande	
 		check_and_execute_formule(cell, operation);	
 	        }
 	 	else
@@ -187,20 +211,26 @@ function () {
 	        	alert("Formule non valide");
 	 	}
 	};
+
 
 
 	//Affiche un repère
 	var plot = function (cell) 
 	{
+		//On récupère ce qu'il y dans l'input de la cellule
 		var text = cell.get_text();
+		//si l'input est vide on indique que le formule n'est pas valide
 		if(text.trim()!="")
 		{
+			//formatage de la chaine en fonction de l'opération à effectuer
 			var operation = "plot("+text+");"
-			/*Création des variables*/
+			//Création des variables 
 	        	create_var(cell);
+			//si l'output n'est pas vide, on le vide
 	        	if(cell.output_area.outputs[0]!=undefined){
 	        		cell.output_area.clear_output();
-			}	
+			}
+		//vérification de la formule et exécution de la commande	
 		check_and_execute_formule(cell, operation);	
 	        }
 	 	else
@@ -209,18 +239,26 @@ function () {
 	 	}
 	};
 
-	//simplifie les fractions
+
+		
+		        		
+	//simplifie les formules
 	var simplify = function (cell) 
 	{
+		//On récupère ce qu'il y dans l'input de la cellule
 		var text = cell.get_text();
+		//si l'input est vide on indique que le formule n'est pas valide
 		if(text.trim()!="")
 		{
+			//formatage de la chaine en fonction de l'opération à effectuer
 			var operation = "("+text+").simplify_full();"
-			/*Création des variables*/
+			//Création des variables
 	        	create_var(cell);
+			//si l'output n'est pas vide, on le vide
 	        	if(cell.output_area.outputs[0]!=undefined){
 	        		cell.output_area.clear_output();
-			}	
+			}
+		//vérification de la formule et exécution de la commande	
 		check_and_execute_formule(cell, operation);	
 	        }
 	 	else
@@ -240,8 +278,8 @@ function () {
 			display_start_button();
 			//Display Advanced Button
 			display_button();
-			/*Ajouter des events*/
-			add_event_on_notebook();	
+			/*Affiche l' input de la cellule*/
+			click_on_button();	
 			// module activé		
 			boolean = true;
 		}
@@ -254,8 +292,8 @@ function () {
 			
 	} 
 	
-	/*add event on a notebook*/
-	var add_event_on_notebook = function(){
+	/*Display input cel*/
+	var click_on_button = function(){
 			//Permet de rafraichir le DOM et d'ajouter nos fonctionnalités
 	$("body").on('click','button[title =\"sageButtonFactor\"]', function() {
 			var cell = Jupyter.notebook.get_cell(Jupyter.notebook.get_selected_cells_indices());
@@ -298,18 +336,18 @@ function () {
 			//On Expand notre entrée
 			expand(cell);
 		});
+	};
 
-};
 	//Display Advanced Button
 	var display_button = function(){
 	// buttons
 
-		var buttons ="<div title=\"sage\" class =\"container\" style=\"border: 5px solid transparent\" ><div class=\"row\"> <div class =\"col-xs-12\"><div class =\"col-xs-1\"><button title =\"sageButtonFactor\"type=\"button\" class=\"btn btn-info\">Factor</button></div><div class =\"col-xs-1\"><button title =\"sageButtonSolve\"type=\"button\" class=\"btn btn-warning\">Solve</button></div><div class =\"col-xs-1\"><button title =\"sageButtonPlot\"type=\"button\" class=\"btn btn-danger\">Plot</button></div><div class =\"col-xs-1\"><button title =\"sageButtonExpand\"type=\"button\" class=\"btn btn-warning\">Expand</button></div><div class =\"col-xs-1\"><button title =\"sageButtonDiff\"type=\"button\" class=\"btn btn-danger\">Diff</button></div></div></div>";
+		var buttons ="<div title=\"sage\" class =\"container\" style=\"border: 5px solid transparent\" ><div class=\"row\"> <div class =\"col-xs-12\"><div class =\"col-xs-1\"><button title =\"sageButtonFactor\"type=\"button\" class=\"btn btn-info\">Factor</button></div><div class =\"col-xs-1\"><button title =\"sageButtonSolve\"type=\"button\" class=\"btn btn-warning\">Solve</button></div><div class =\"col-xs-1\"><button title =\"sageButtonPlot\"type=\"button\" class=\"btn btn-danger\">Plot</button></div><div class =\"col-xs-1\"><button title =\"sageButtonExpand\"type=\"button\" class=\"btn btn-warning\">Expand</button></div><div class =\"col-xs-1\"><button title =\"sageButtonDiff\"type=\"button\" class=\"btn btn-danger\">Diff</button></div><div class =\"col-xs-1\"><button title =\"sageButtonSimplify\"type=\"button\" class=\"btn btn-info\">Simplify</button></div></div></div>";
 
 		// check the focus
 		$('body').on('focusin', '.selected .input_area', function(){
 			// check if the sage butons is write
-			$('.unselected > div[title =\"sage\"').remove();		
+				$('.unselected > div[title =\"sage\"').remove();		
 			if($('.selected > div[title !=\"sage\"]')){
 				// supprimer div 
 				$('.selected > div[title =\"sage\"').remove();
@@ -368,11 +406,3 @@ function () {
 		load_ipython_extension: load_ipython_extension
 	};
 });
-
-
-
-
-
-
-
-
